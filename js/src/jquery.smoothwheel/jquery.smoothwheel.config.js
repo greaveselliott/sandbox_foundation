f83bb5b30ef19761dd44ee206efb05a9a2f39c48
Elliott-Greaves <greaveselliott@me.com>
2015-11-15 22:56:18 +0000

    removed unecessary files

--- js/src/jquery.smoothwheel/jquery.smoothwheel.config.js
+++
@@ -1,3 +0,0 @@
-﻿(function ($) {
-    $("body").smoothWheel();
-})(jQuery);
--- js/src/slick/slick.config.js
+++
@@ -1,7 +0,0 @@
-(function ($) {
-    $('.carousel').slick({
-        lazyLoad: 'ondemand',
-        prevArrow: '<div class="button prev"><i class="flaticon-prev"></i></div>',
-        nextArrow: '<div class="button next"><i class="flaticon-next"></i></div>'
-    });
-})(jQuery);
